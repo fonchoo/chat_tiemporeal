fa12d9abf65ea1cee10215df3aae3831c41715db
Patricio Finschi <patofinschi@gmail.com>
2025-10-03 23:32:13 -0300

    Configuración de puerto

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+const port = proccess.env.PORT || 3000;
+server.listen(port, () => console.log(`Servidor en puerto ${port}`));
+
 app.use(express.static('public'));
 
 let usuarios = {};
